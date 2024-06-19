9d54e482a88fd3588c1f097d42b7c53dceb0ff95
覃昶栋 <qcd1782@gmail.com>
2024-06-19 02:28:49 +0000

    System generated commit, initial commit

---
+++ pages/Movies/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,14 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		this.myVar1 = [1,2,3];
+		let abc = 1+1
+		return abc
+	},
+	async myFun2 () {
+		//	use async-await or promises
+			await storeValue('cc', 'hello world')
+	}
+}
